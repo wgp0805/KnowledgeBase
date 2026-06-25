d2fe996e1d0e85e8ae0b64ae46816d0d65556e52
srd18621992017 <xufei@ideal.sh.cn>
2026-06-25 10:00:47 +0800

    vault backup: 2026-06-25 10:00:47

---
+++ scripts/_fix_encoding.py
@@ -0,0 +1,3 @@
+import sys, io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8", errors="replace")
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding="utf-8", errors="replace")
